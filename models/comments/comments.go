83a43038703f5902d01270e59199a5479dfb232a
Samuyi <pandorra90@gmail.com>
2018-07-22 16:34:28 +0100

    refactored comment model to use redis

--- models/comments/comments.go
+++ models/comments/comments.go
@@ -1,117 +1,293 @@
 package comments
 
 import (
-	"database/sql"
 	"log"
+	"strconv"
 	"time"
 
-	_ "github.com/lib/pq" // postgres driver
+	"github.com/go-redis/redis"
+	uuid "github.com/satori/go.uuid"
 )
+
+var client *redis.Client
+
+func init() {
+	client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+}
 
 //Comment is the data structure of a comment on an item
 type Comment struct {
-	ID        string
-	ItemID    string
-	UserID    string
-	Comment   string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID         string
+	ItemID     string
+	Username   string
+	Comment    string
+	ReplyCount int64
+	Replies    []Reply
+	CreatedAt  float64
+	UpdatedAt  int64
 }
 
-var db *sql.DB
-
-func init() {
-	var err error
-
-	db, err = sql.Open("postgres", "user=help.ng passowrd=this is the password for help.ng sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
+//Reply a comment by a user
+type Reply struct {
+	ID        string
+	Username  string
+	CommentID string
+	Comment   string
+	CreatedAt float64
 }
 
 //Create a comment for an item
 func (comment *Comment) Create() error {
-	query := "INSERT INTO items (item_id, user_id, comment) values ($1, $2, $3,)returning id"
+	comment.ID = uuid.Must(uuid.NewV4()).String()
+	comment.CreatedAt = float64(time.Now().Unix())
+	comment.UpdatedAt = 0
 
-	stmt, err := db.Prepare(query)
-	defer stmt.Close()
+	var z redis.Z
+	z.Score = comment.CreatedAt
+	z.Member = comment.ID
 
-	if err != nil {
-		log.Fatal(err)
-		return err
+	pipeline := client.Pipeline()
+
+	pipeline.ZAdd(comment.ItemID, z) // add a comment id to a zset representing a an item
+
+	fields := map[string]interface{}{
+		"username":   comment.Username,
+		"comment":    comment.Comment,
+		"created_at": comment.CreatedAt,
+		"updated_at": comment.UpdatedAt,
 	}
 
-	err = stmt.QueryRow(comment.ItemID, comment.UserID, comment.Comment).Scan(&comment.ID)
+	pipeline.HMSet(comment.ID, fields)
+
+	_, err := pipeline.Exec()
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return err
 	}
+	return nil
 
-	return err
 }
 
-//Update a comment for an item
-func (comment *Comment) Update() error {
-	query := "UPDATE comment SET comment = $1 where id = $2"
+//Create a reply to a comment by a user
+func (reply *Reply) Create() error {
+	reply.ID = uuid.Must(uuid.NewV4()).String()
+	reply.CreatedAt = float64(time.Now().Unix())
 
-	stmt, err := db.Prepare(query)
-	defer stmt.Close()
+	fields := map[string]interface{}{
+		"username":   reply.Username,
+		"comment":    reply.Comment,
+		"created_at": reply.CreatedAt,
+	}
+
+	z := redis.Z{Score: reply.CreatedAt, Member: reply.ID}
+
+	pipeline := client.Pipeline()
+	pipeline.ZAdd("replies:"+reply.CommentID, z)
+	pipeline.HMSet(reply.ID, fields)
+	_, err := pipeline.Exec()
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return err
 	}
 
-	_, err = stmt.Exec(comment.Comment)
+	return nil
+
+}
+
+//Get a reply
+func (reply *Reply) Get() error {
+	resp, err := client.HGetAll(reply.ID).Result()
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return err
 	}
-	return err
+
+	reply.Comment = resp["comment"]
+	reply.CreatedAt, _ = strconv.ParseFloat(resp["created_at"], 64)
+	reply.Username = resp["username"]
+
+	return nil
 }
 
 //Get a comment from database
 func (comment *Comment) Get() error {
-	query := "SELECT comment, user_id, created_at, item_id from comments where id = $1"
-
-	stmt, err := db.Prepare(query)
-	defer stmt.Close()
-
+	resp, err := client.HGetAll(comment.ID).Result()
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return err
 	}
 
-	err = stmt.QueryRow(comment.ID).Scan(&comment.Comment, &comment.UserID, &comment.CreatedAt, &comment.ItemID)
+	replyCount, err := client.ZCount("replies:"+comment.ID, "-inf", "+inf").Result()
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return err
 	}
 
-	return err
+	comment.Username = resp["username"]
+	comment.Comment = resp["comment"]
+	comment.ReplyCount = replyCount
+
+	return nil
+
+}
+
+//GetReplies to a comment
+func (comment *Comment) GetReplies() error {
+
+	opt := redis.ZRangeBy{Min: "-inf", Max: "+inf"}
+
+	resp, err := client.ZRangeByScoreWithScores("replies:"+comment.ID, opt).Result()
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	for _, v := range resp {
+		var reply Reply
+		id := v.Member.(string)
+		res, err := client.HGetAll(id).Result()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		reply.Username = res["username"]
+		reply.Comment = res["comment"]
+		reply.ID = id
+		reply.CreatedAt = v.Score
+
+		comment.Replies = append(comment.Replies, reply)
+
+	}
+	return nil
+
 }
 
 //Delete a comment from the database
 func (comment *Comment) Delete() error {
-	query := "DELETE FROM comments where id = $id"
+	pipeline := client.Pipeline()
 
-	stmt, err := db.Prepare(query)
-	defer stmt.Close()
+	pipeline.ZRem(comment.ItemID, comment.ID)
+	pipeline.Del(comment.ID)
+	_, err := pipeline.Exec()
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return err
 	}
 
-	_, err = stmt.Exec(comment.ID)
+	opt := redis.ZRangeBy{Min: "-inf", Max: "+inf"}
+	resp, err := client.ZRangeByScoreWithScores("replies:"+comment.ID, opt).Result()
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return err
 	}
 
-	return err
+	for _, v := range resp {
+		id := v.Member.(string)
+		pipeline.Del(id)
+	}
+
+	_, err = pipeline.Exec()
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+//Delete a reply
+func (reply *Reply) Delete() error {
+	pipeline := client.Pipeline()
+	pipeline.ZRem("replies:"+reply.CommentID, reply.ID)
+	pipeline.Del(reply.ID)
+	_, err := pipeline.Exec()
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+//Update a comment
+func (comment *Comment) Update() error {
+	updatedAt := time.Now().Unix()
+	fields := map[string]interface{}{
+		"comment":    comment.Comment,
+		"updated_at": updatedAt,
+	}
+	_, err := client.HMSet(comment.ID, fields).Result()
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+//Update a reply to a comment
+func (reply *Reply) Update() error {
+	updatedAt := time.Now().Unix()
+
+	fields := map[string]interface{}{
+		"comment":    reply.Comment,
+		"updated_at": updatedAt,
+	}
+
+	_, err := client.HMSet(reply.ID, fields).Result()
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+//GetItemComments gets all comments associated with an item
+func (comment *Comment) GetItemComments() ([]Comment, error) {
+	opts := redis.ZRangeBy{
+		Max: "+inf",
+		Min: "-inf",
+	}
+	resp, err := client.ZRangeByScoreWithScores(comment.ItemID, opts).Result()
+
+	if err != nil {
+		log.Panicln(err)
+		return nil, err
+	}
+
+	var comments []Comment
+
+	for _, v := range resp {
+		var comment Comment
+		id := v.Member.(string)
+		comment.ID = id
+		comment.CreatedAt = v.Score
+		err := comment.Get()
+
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		comments = append(comments, comment)
+	}
+
+	return comments, nil
+
 }
